950701dc625b531bb5296181e20fb8e8b94b20fd
BuildTools <unconfigured@null.spigotmc.org>
2022-04-02 16:28:30 +1100

    Initialize Repository

---
+++ Orders.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+require 'json'
+
+class Orders
+
+    def processOrders()
+
+        puts "code initialized "
+    end
+
+end
+
+# Call and run program
+run = Orders.new
+run.processOrders
